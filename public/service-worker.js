b9e2c3e0972088b35f17cb9f34a20b34fc1c913f
Justin Oeters <justin.oeters@gmail.com>
2021-12-04 20:41:54 -0500

    added caching functionality

---
+++ public/service-worker.js
@@ -0,0 +1,88 @@
+// Files Cache
+
+const { request } = require("express");
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    //Database js
+    "/",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install the cache
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches
+        .open(CACHE_NAME)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+});
+
+// clearing out old caches
+
+self.addEventListener("activate", event => {
+    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+    event.waitUntil(
+        caches
+        .keys()
+        .then(cacheNames => {
+            return cacheNames.filter(
+                cacheName => !currentCaches.includes(cacheName)
+            );
+        })
+        .then(cachesToDelete => {
+            return Promise.all(
+                cachesToDelete.map(cacheToDelete => {
+                    return caches.delete(cacheToDelete);
+                })
+            );
+        })
+        .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (
+        event.request.method !== "GET" ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+                .catch(() => caches.match(event.request));
+            })
+        );
+        return;
+    }
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, respone.clone()).then(() =>{
+                        return response;
+                    });
+                });
+            });
+        })
+    );
+});
